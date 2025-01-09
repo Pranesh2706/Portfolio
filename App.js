bf753f1877d4f21000a3ea4da9b2f5ce0573b9e9
Pranesh2706 <148603006+Pranesh2706@users.noreply.github.com>
2025-01-09 12:40:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./Components/Header/Header";
+import TopContainer from "./Components/TopContainer/TopContainer";
+
+
+
+const App = () => {
+  return (
+
+    <div>
+      <Header/>
+      <TopContainer/>
+    </div>
+
+  );
+};
+export default App
